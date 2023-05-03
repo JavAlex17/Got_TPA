fb385b686e77750bcdaf6fca13822fd32ee7fbac
JavAlex17 <72239869+JavAlex17@users.noreply.github.com>
2023-05-02 22:03:39 -0400

    TablaPersonajes

--- lectordatos.py
+++ lectordatos.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QMessageBox, QGridLayout, QPushButton, QStackedLayout, QVBoxLayout, QHBoxLayout, QWidget, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget , QTableWidget, QTableWidgetItem
 
 
 datos=list(pd.read_csv('character_deaths.csv',header=0))
@@ -12,15 +12,15 @@ class VentanaPersonajes(QMainWindow):
     def __init__(self, archivo):
         super().__init__()
         
-        self.table_widget = QTableWidget(self)
+        self.tablePersonajes = QTableWidget(10,13)
         
-        n = 0
-        for linea in archivo:
-            item = QTableWidgetItem(linea)
-            self.table_widget.append(item)
+        self.tablePersonajes.setHorizontalHeaderLabels(['Name','Allegiances','Death Year','Book of Death','Death Chapter','Book Intro Chapter','Gender','Nobility','GoT','CoK','SoS','FfC','DwD'])
+        
+        
+        #self.table_widget = QTableWidget(self)
             
         self.setWindowTitle("Muertes de Personajes")
-        self.setCentralWidget(self.table_widget)
+        self.setCentralWidget(self.tablePersonajes)
         
 app = QApplication(sys.argv)
 ventana = VentanaPersonajes(datos)
